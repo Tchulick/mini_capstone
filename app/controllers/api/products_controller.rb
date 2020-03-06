6564900754ad8fa06394d6eb83d4bb8f516f10d6
Tom <tchulick48@gmail.com>
2020-03-05 19:58:43 -0600

    Created update and destroy methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order(:id)
     render "index.json.jb"
   end
 
@@ -17,6 +17,22 @@ class Api::ProductsController < ApplicationController
 
   def show
     @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
     render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get "/products" => "products#index"
     post "/product" => "products#create"
     get "/product/:id" => "products#show"
+    patch "/product/:id" => "products#update"
+    delete "/product/:id" => "products#destroy"
   end
 end
