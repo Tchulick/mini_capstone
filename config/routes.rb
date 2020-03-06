cacb53ce6f45c87a53a9bcd65ec00ec0103effa5
Tom <tchulick48@gmail.com>
2020-03-05 18:59:42 -0600

    Added a create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/product/:id" => "products#show"
     get "/products" => "products#index"
+    post "/product" => "products#create"
+    get "/product/:id" => "products#show"
   end
 end
