672fe504a6391c5c7c6e3c303ae11c8068b0cdd5
Tom <tchulick48@gmail.com>
2020-03-08 09:06:29 -0500

    Small modifications and renaming multiple files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
-    post "/product" => "products#create"
-    get "/product/:id" => "products#show"
-    patch "/product/:id" => "products#update"
-    delete "/product/:id" => "products#destroy"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
   end
 end
