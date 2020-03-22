557d0f081f0c5b69b43e99538a6d6aca68993f7f
Tom <tchulick48@gmail.com>
2020-03-22 09:33:31 -0500

    Updated relationships between models

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ApplicationRecord
   end
 
   has_many :images
-  has_many :orders
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, through: :category_products
   has_many :carted_products
